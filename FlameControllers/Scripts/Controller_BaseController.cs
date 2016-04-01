d62ca84b4df1d9fa29cc3e4a8a95fa41aa170e61
Alexander Björkman <ab3.git@eit.se>
2016-04-01 20:26:46 +0200

    Added litjson

--- FlameControllers/Scripts/Controller_BaseController.cs
+++ FlameControllers/Scripts/Controller_BaseController.cs
@@ -13,7 +13,7 @@ public class Controller_BaseController : MonoBehaviour {
 	[ReadOnlyAttribute] public GameObject avatar;
 	
 	// The avatar that is used in the begining.
-	[SerializeField] private GameObject startAvatar = null; 
+	[SerializeField] private GameObject startAvatar = this.g; 
 	
 	// Rigidbody for the avatar.
 	private Rigidbody avatar_rigidbody;
