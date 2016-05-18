1b18508048015b986628d2d54687bc2caf8216ba
Alexander Björkman <ab3.git@eit.se>
2016-05-18 17:59:36 +0200

    Added Attr

---
+++ FlameAttr/Flame_Attr.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+[Serializable]
+public class Flame_Attr : ICloneable, ISerializationCallbackReceiver
+{
+
+	[SerializeField]
+	private List<string> _keys;
+
+	[SerializeField]
+	private List<string> _values;
+
+	public Dictionary<string, string> content = new Dictionary<string, string>();
+
+	public Flame_Attr()
+	{
+
+	}
+
+	public void OnBeforeSerialize()
+	{
+		_keys.Clear();
+		_values.Clear();
+		foreach (var kvp in content)
+		{
+			_keys.Add(kvp.Key);
+			_values.Add(kvp.Value);
+		}
+	}
+
+	public void OnAfterDeserialize()
+	{
+		content.Clear();
+		for (int i = 0; i != Math.Min(_keys.Count, _values.Count); i++)
+			content.Add(_keys[i], _values[i]);
+	}
+
+	void OnGUI()
+	{
+
+		foreach (var kvp in content)
+			GUILayout.Label("Key: " + kvp.Key + " value: " + kvp.Value);
+	}
+
+	private static string addTwoStringEntries(string a, string b)
+	{
+		// Try to convert a
+		float j;
+		float k;
+
+		if (float.TryParse(a, out j) && float.TryParse(b, out k))
+		{
+			return "" + (j + k);
+		}
+		else
+		{
+			return a + b;
+		}
+	}
+
+	public static Flame_Attr operator +(Flame_Attr a1, Flame_Attr a2)
+	{
+		Flame_Attr a3 = a1.Clone();
+		
+
+		foreach(var entry in a1.content)
+		{
+			a3.content[entry.Key] = addTwoStringEntries(a1.content[entry.Key], a2.content[entry.Key]);
+		}
+
+		return a3;
+
+	}
+
+	public Flame_Attr Clone()
+	{
+		Flame_Attr a1 = new Flame_Attr();
+		a1.content = new Dictionary<string, string>(this.content.Count, this.content.Comparer);
+
+		foreach (KeyValuePair<string, string> entry in this.content)
+		{
+			a1.content.Add(entry.Key, (string)entry.Value.Clone());
+		}
+
+		return a1;
+	}
+
+	object ICloneable.Clone()
+	{
+		return Clone();
+	}
+}
---
+++ FlameUtil/Scripts/Editor/Flame_AttrDrawer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(Flame_Attr))]
+public class Flame_AttrEditor : PropertyDrawer
+{
+
+	string valText = "Value";
+	string keyText = "Key";
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		if (property.isExpanded)
+			return EditorGUI.GetPropertyHeight(property) + 80f;
+		return EditorGUI.GetPropertyHeight(property);
+	}
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+
+		EditorGUI.PropertyField(position, property, label, true);
+		if (property.isExpanded)
+		{
+			Rect r1 = new Rect(position.xMin + 60f, position.yMax - 40f, (position.width - 30f) / 2f, 20f);
+			Rect r2 = new Rect(position.xMin + 60f + (position.width - 30f) / 2f, position.yMax - 40f, (position.width - 30f) / 2f, 20f);
+			Rect r3 = new Rect(position.xMin + 60f, position.yMax - 20f, position.width - 30f, 20f);
+
+			Rect a1 = new Rect(position.xMin + 60f, position.yMax - 60f, (position.width - 30f) / 2f, 20f);
+			Rect a2 = new Rect(position.xMin + 60f + (position.width - 30f) / 2f, position.yMax - 60f, (position.width - 30f) / 2f, 20f);
+
+			EditorGUI.LabelField(a1, "Key");
+			EditorGUI.LabelField(a2, "Value");
+
+			valText = EditorGUI.TextField(r2, valText);
+			keyText = EditorGUI.TextField(r1, keyText);
+
+			if (GUI.Button(r3,"Add Entry"))
+			{
+				Flame_Attr attr = fieldInfo.GetValue(property.serializedObject.targetObject) as Flame_Attr;
+				attr.content[keyText] = valText;
+
+
+			}
+		}
+	}
+}
